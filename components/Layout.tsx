092592481e9a382003fc2d66b178931fd0a9b9fd
Harun Doğdu <harundogdu06@gmail.com>
2023-03-29 01:31:51 +0300

    feat: the template of the project is designed

---
+++ components/Layout.tsx
@@ -0,0 +1,40 @@
+import React, {FC} from "react";
+import Sidebar from "@/components/Sidebar";
+
+interface ILayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout: FC<ILayoutProps> = ({children}) => {
+    return (
+        <div className="
+        h-screen
+        bg-black
+        ">
+            <div className="
+            h-full
+            mx-auto
+            xl:px-30
+            max-w-[78rem]
+            ">
+                <div className="
+                    grid
+                    grid-cols-4
+                    h-full
+                ">
+                    <Sidebar />
+                    <div className="
+                    col-span-3
+                    lg:col-span-2
+                    border-x-[1px]
+                    border-neutral-800
+                    ">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
