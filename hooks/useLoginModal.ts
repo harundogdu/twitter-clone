0317088ec2b9c126e93e7e08fb5a69b5e14c0c63
Harun Doğdu <harundogdu06@gmail.com>
2023-03-29 11:55:55 +0300

    feat: useLoginModal hook created

---
+++ hooks/useLoginModal.ts
@@ -0,0 +1,15 @@
+import {create} from "zustand";
+
+interface LoginModalState {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+}
+
+const useLoginModal = create<LoginModalState>((set) => ({
+    isOpen: false,
+    onClose: () => set({isOpen: false}),
+    onOpen: () => set({isOpen: true}),
+}));
+
+export default useLoginModal;
