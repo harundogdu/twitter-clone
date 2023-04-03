c33dd24f37729fa6f01d7590c2e1f3e8153db230
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-04 01:48:33 +0300

    feat: logout sidebar item added

--- utils/@fake.db.ts
+++ utils/@fake.db.ts
@@ -1,54 +1,58 @@
-import {ISidebarType} from "@/types/sidebar.type";
+import { ISidebarType } from "@/types/sidebar.type";
+import { CiCircleMore } from "react-icons/ci";
 import {
-    RiBookmarkLine,
-    RiHashtag,
-    RiHome7Fill,
-    RiMailLine,
-    RiNotification3Line,
-    RiSearchLine,
-    RiUserLine
+  RiBookmarkLine,
+  RiHashtag,
+  RiHome7Fill,
+  RiLogoutBoxLine,
+  RiMailLine,
+  RiNotification3Line,
+  RiSearchLine,
+  RiUserLine,
 } from "react-icons/ri";
-import {CiCircleMore} from "react-icons/ci";
 
 const SidebarItems: ISidebarType[] = [
-    {
-        label: "Home",
-        href: "/",
-        icon: RiHome7Fill,
-    },
-    {
-        label: "Explore",
-        href: "/explore",
-        icon: RiHashtag,
-        secondaryIcon: RiSearchLine,
-    },
-    {
-        label: "Notifications",
-        href: "/notifications",
-        icon: RiNotification3Line,
-    },
-    {
-        label: "Messages",
-        href: "/messages",
-        icon: RiMailLine,
-    },
-    {
-        label: "Bookmarks",
-        href: "/bookmarks",
-        icon: RiBookmarkLine,
-    },
-    {
-        label: "Profile",
-        href: "/profile/hd",
-        icon: RiUserLine,
-    },
-    {
-        label: "More",
-        href: "/more",
-        icon: CiCircleMore,
-    }
+  {
+    label: "Home",
+    href: "/",
+    icon: RiHome7Fill,
+  },
+  {
+    label: "Explore",
+    href: "/explore",
+    icon: RiHashtag,
+    secondaryIcon: RiSearchLine,
+  },
+  {
+    label: "Notifications",
+    href: "/notifications",
+    icon: RiNotification3Line,
+  },
+  {
+    label: "Messages",
+    href: "/messages",
+    icon: RiMailLine,
+  },
+  {
+    label: "Bookmarks",
+    href: "/bookmarks",
+    icon: RiBookmarkLine,
+  },
+  {
+    label: "Profile",
+    href: "/profile/hd",
+    icon: RiUserLine,
+  },
+  {
+    label: "More",
+    href: "/more",
+    icon: CiCircleMore,
+  },
+  {
+    label: "Logout",
+    href: "/logout",
+    icon: RiLogoutBoxLine,
+  },
 ];
 
-export {
-    SidebarItems
-};
+export { SidebarItems };
