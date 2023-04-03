53f950795e351b4c8b54e4f308fa562b062254e1
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-04 01:31:30 +0300

    feat: auto-animate library added to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,27 @@
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
-import Layout from "@/components/Layout";
-import Modal from "@/components/shared/Modal";
-import ColorUtils from "@/base/colors";
-import {useState} from "react";
-import LoginModal from "@/components/modals/LoginModal";
+import type { AppProps } from "next/app";
 
-export default function App({Component, pageProps}: AppProps) {
-    return (
-        <>
-            <LoginModal />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </>
-    );
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [animationParent] = useAutoAnimate();
+  return (
+    <>
+      <Head>
+        <title>Twitter 2.0 - HD</title>
+      </Head>
+      <main ref={animationParent}>
+        <RegisterModal />
+        <LoginModal />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
+  );
 }
