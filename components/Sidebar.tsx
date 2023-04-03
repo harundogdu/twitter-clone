4f289d068c62f3aa0a7ba56162e1ee9d354db067
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-04 01:48:21 +0300

    feat: sidebar style fixed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,124 +1,83 @@
-import {useCallback} from "react";
+import { useCallback } from "react";
 import Image from "next/image";
 
-import {RiMoreFill} from "react-icons/ri";
+import { RiMoreFill } from "react-icons/ri";
 
 import ColorUtils from "@/base/colors";
 import SpaceUtils from "@/base/spaces";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useWindowSize from "@/hooks/useWindowSize";
-import {SidebarItems} from "@/utils/@fake.db";
+import { SidebarItems } from "@/utils/@fake.db";
 
 import SidebarLogo from "@/components/SidebarLogo";
 import SidebarItem from "@/components/SidebarItem";
 import Button from "@/components/shared/Button";
 
 const Sidebar = () => {
-    const {width} = useWindowSize();
-    const {onOpen} = useLoginModal();
-    const RenderSidebarItems = useCallback(() => {
-        return (
-            <>
-                {
-                    SidebarItems.map((item, index) => (
-                        <SidebarItem
-                            label={item.label}
-                            icon={item.icon}
-                            secondaryIcon={item.secondaryIcon}
-                            href={item.href}
-                            onClick={item.onClick}
-                            key={index}
-                        />
-                    ))
-                }
-            </>
-        );
-    }, []);
+  const { width } = useWindowSize();
+  const { onOpen } = useLoginModal();
+  const RenderSidebarItems = useCallback(() => {
     return (
-        <div className="
-            mt-[0.875rem]
-            px-1
-            h-full
-            col-span-1
-            sm:px-4
-            md:px-6
-            flex
-            items-start
-            justify-center
-        ">
-            <div className="
-            flex
-            flex-col
-            items-center
-            md:items-start
-            h-full
-           "
-            >
-                <div className="
-                flex
-                flex-col
-                h-full
-                justify-between
-             ">
-                    <div className="
-                space-y-2.5
-                lg:w-[230px]
-                self-center
-             ">
-                        <SidebarLogo/>
-                        <RenderSidebarItems/>
-                        <Button
-                            label={"Share"}
-                            fullWidth
-                            size={"lg"}
-                            bgColor={ColorUtils.colors.purple}
-                            color={ColorUtils.colors.white}
-                            marginVertical={SpaceUtils.spaces.lg}
-                            showShareButton
-                            onClick={onOpen}
-                        />
-                    </div>
-                    <div className="
-                    flex
-                    gap-5
-                    items-center
-                    justify-center
-                    rounded-full
-                    cursor-pointer
-                    hover:bg-neutral-800
-                    hover:bg-opacity-70
-                    mb-10
-                    p-2
-                    px-4
-                    transition-colors
-                    ">
-                        <div>
-                            <Image src={"https://avatars.githubusercontent.com/u/43449361?v=4"} alt={"avatar"}
-                                   width={36}
-                                   height={36}
-                            />
-                        </div>
-                        {width! > 1024 && (
-                            <>
-                                <div className="
-                        flex
-                        flex-col
-                        ">
-                                    <div className="text-white font-bold">Harun Doğdu <span>🇹🇷</span></div>
-                                    <div className="text-gray-500">@harunndogdu</div>
-                                </div>
-                                <div>
-                                    <RiMoreFill size={28} color="#fff"/>
-                                </div>
-                            </>
-                        )
-                        }
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className="space-y-1">
+        {SidebarItems.map((item, index) => (
+          <SidebarItem
+            label={item.label}
+            icon={item.icon}
+            secondaryIcon={item.secondaryIcon}
+            href={item.href}
+            onClick={item.onClick}
+            key={index}
+          />
+        ))}
+      </div>
     );
+  }, []);
+  return (
+    <div className="mt-[0.875rem] px-1 h-full col-span-1 sm:px-4 md:px-6 flex items-start justify-center">
+      <div className="flex flex-col items-center md:items-start h-full">
+        <div className="flex flex-col h-full justify-between">
+          <div className="space-y-2.5 lg:w-[230px] self-center">
+            <SidebarLogo />
+            <RenderSidebarItems />
+            <Button
+              label={"Share"}
+              fullWidth
+              size={"lg"}
+              bgColor={ColorUtils.colors.purple}
+              color={ColorUtils.colors.white}
+              marginVertical={SpaceUtils.spaces.lg}
+              showShareButton
+              onClick={onOpen}
+            />
+          </div>
+          <div className="flex gap-5 items-center justify-center rounded-full cursor-pointer hover:bg-neutral-800 hover:bg-opacity-70 mb-10 transition-colors p-2">
+            <div>
+              <Image
+                src={"https://avatars.githubusercontent.com/u/43449361?v=4"}
+                alt={"avatar"}
+                width={32}
+                height={32}
+              />
+            </div>
+            {width! > 1024 && (
+              <>
+                <div className="flex flex-col items-start justify-center">
+                  <div className="text-white font-bold text-sm">
+                    Harun Doğdu <span>🇹🇷</span>
+                  </div>
+                  <div className="text-gray-500 text-sm">@harunndogdu</div>
+                </div>
+                <div>
+                  <RiMoreFill size={24} color="#fff" />
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,77 +1,39 @@
-import {ISidebarType} from "@/types/sidebar.type";
-import {FC, useCallback} from "react";
+import { ISidebarType } from "@/types/sidebar.type";
+import { FC, useCallback } from "react";
 import useWindowSize from "@/hooks/useWindowSize";
 
-const SidebarItem: FC<ISidebarType> = (
-    {
-        href,
-        onClick,
-        label,
-        icon: Icon,
-        secondaryIcon: SecondaryIcon
-    }) => {
-    const {width} = useWindowSize();
+const SidebarItem: FC<ISidebarType> = ({
+  href,
+  onClick,
+  label,
+  icon: Icon,
+  secondaryIcon: SecondaryIcon,
+}) => {
+  const { width } = useWindowSize();
 
-    const RenderIcon = useCallback(() => {
-        return (
-            width! < 1024 ? (
-                SecondaryIcon ? <SecondaryIcon size={28} color="#fff"/> :
-                    <Icon size={28} color="#fff"/>
-            ) : (
-                <Icon size={28} color="#fff"/>
-            )
-        );
-    }, [width, SecondaryIcon, Icon]);
-
-    return (
-        <div className="
-            flex
-            items-center
-            flex-row
-       "
-        >
-            <div className="
-            relative
-            rounded-full
-            h-14
-            w-14
-            p-4
-            flex
-            items-center
-            justify-center
-            cursor-pointer
-            hover:bg-neutral-800
-            hover:bg-opacity-70
-            transition
-            lg:hidden
-        "
-            >
-                <RenderIcon/>
-            </div>
-            <div className="
-            relative
-            hidden
-            lg:flex
-            items-center
-            rounded-full
-            cursor-pointer
-             hover:bg-neutral-800
-            hover:bg-opacity-70
-            gap-4
-            p-4
-           ">
-                <Icon size={28} color="#fff"/>
-                <span className="
-                text-white
-                text-xl
-                hidden
-                lg:block
-               ">
-                     {label}
-               </span>
-            </div>
-        </div>
+  const RenderIcon = useCallback(() => {
+    return width! < 1024 ? (
+      SecondaryIcon ? (
+        <SecondaryIcon size={28} color="#fff" />
+      ) : (
+        <Icon size={28} color="#fff" />
+      )
+    ) : (
+      <Icon size={28} color="#fff" />
     );
+  }, [width, SecondaryIcon, Icon]);
+
+  return (
+    <div className="flex items-center flex-row">
+      <div className="relative rounded-full h-14 w-14 p-4 flex items-center justify-center cursor-pointer hover:bg-neutral-800 hover:bg-opacity-70 transition lg:hidden">
+        <RenderIcon />
+      </div>
+      <div className="relative hidden lg:flex items-center rounded-full cursor-pointer hover:bg-neutral-800 hover:bg-opacity-70 gap-4 p-4">
+        <Icon size={28} color="#fff" />
+        <span className="text-white text-xl hidden lg:block">{label}</span>
+      </div>
+    </div>
+  );
 };
 
 export default SidebarItem;
