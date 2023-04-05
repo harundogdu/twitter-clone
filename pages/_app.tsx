2fb1bcb47298602bdf4cb1ca29a5f3a5f325f5e2
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-05 03:59:04 +0300

    feat: session provider and Toaster methods added to the _app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,32 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 import Layout from "@/components/Layout";
+
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [animationParent] = useAutoAnimate();
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <title>Twitter 2.0 - HD</title>
       </Head>
       <main ref={animationParent}>
+        <Toaster toastOptions={{ duration: 2000, position: "bottom-right" }} />
         <RegisterModal />
         <LoginModal />
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </main>
-    </>
+    </SessionProvider>
   );
 }
