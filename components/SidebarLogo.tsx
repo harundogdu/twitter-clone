0e7e63ddae6f56c60d7ddcd596b31d663bfb24e4
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-05 03:57:12 +0300

    ui: twitter logo styling

--- components/SidebarLogo.tsx
+++ components/SidebarLogo.tsx
@@ -1,26 +1,19 @@
-import {FaTwitter} from "react-icons/fa";
-import {useRouter} from "next/router";
+import { FaTwitter } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 const SidebarLogo = () => {
-    const router = useRouter();
-    const handleOnClick = () => {
-        router.push("/");
-    };
-    return (
-        <div className="
-        text-gray-300
-        hover:text-white
-        transition
-        duration-200
-        cursor-pointer
-        px-4
-        pb-4
-        "
-             onClick={handleOnClick}
-        >
-            <FaTwitter size={28}/>
-        </div>
-    );
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push("/");
+  };
+  return (
+    <div
+      className="text-gray-300 hover:text-white cursor-pointer mx-1    hover:bg-neutral-800 hover:bg-opacity-70  rounded-full transition duration-200 w-fit p-3"
+      onClick={handleOnClick}
+    >
+      <FaTwitter size={32} />
+    </div>
+  );
 };
 
 export default SidebarLogo;
