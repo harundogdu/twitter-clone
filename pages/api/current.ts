f82be16bcf1134796359c715fc9d6c84ee3a897c
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-05 03:53:55 +0300

    feat: current api endpoint created for login user

---
+++ pages/api/current.ts
@@ -0,0 +1,22 @@
+import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req);
+    return res.status(200).json({
+      success: false,
+      currentUser,
+    });
+  } catch (error: any) {
+    console.log(error);
+    return res.status(401).end();
+  }
+}
