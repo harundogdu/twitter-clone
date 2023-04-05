ad35faf443617ed5c9eb5b030e4cc66bfd76b511
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-05 03:58:24 +0300

    feat: data current user improved in the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,19 +8,34 @@ import SpaceUtils from "@/base/spaces";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useWindowSize from "@/hooks/useWindowSize";
-import { SidebarItems } from "@/utils/@fake.db";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 import SidebarLogo from "@/components/SidebarLogo";
 import SidebarItem from "@/components/SidebarItem";
 import Button from "@/components/shared/Button";
 
+import { SidebarItems } from "@/utils/@fake.db";
+
 const Sidebar = () => {
   const { width } = useWindowSize();
   const { onOpen } = useLoginModal();
+  const { data } = useCurrentUser();
+
+  const handleShareClick = useCallback(() => {
+    if (!data?.currentUser?.email) {
+      return onOpen();
+    }
+
+    // TODO:: Share Popup
+  }, [data, onOpen]);
+
   const RenderSidebarItems = useCallback(() => {
+    const sideBarItems = data?.currentUser?.email
+      ? [...SidebarItems]
+      : [...SidebarItems].filter((item) => item.public);
     return (
       <div className="space-y-1">
-        {SidebarItems.map((item, index) => (
+        {sideBarItems.map((item, index) => (
           <SidebarItem
             label={item.label}
             icon={item.icon}
@@ -32,7 +47,7 @@ const Sidebar = () => {
         ))}
       </div>
     );
-  }, []);
+  }, [data]);
   return (
     <div className="mt-[0.875rem] px-1 h-full col-span-1 sm:px-4 md:px-6 flex items-start justify-center">
       <div className="flex flex-col items-center md:items-start h-full">
@@ -41,39 +56,44 @@ const Sidebar = () => {
             <SidebarLogo />
             <RenderSidebarItems />
             <Button
-              label={"Share"}
+              label="Share"
               fullWidth
-              size={"lg"}
+              size="lg"
               bgColor={ColorUtils.colors.purple}
               color={ColorUtils.colors.white}
               marginVertical={SpaceUtils.spaces.lg}
               showShareButton
-              onClick={onOpen}
+              onClick={handleShareClick}
             />
           </div>
-          <div className="flex gap-5 items-center justify-center rounded-full cursor-pointer hover:bg-neutral-800 hover:bg-opacity-70 mb-10 transition-colors p-2">
-            <div>
-              <Image
-                src={"https://avatars.githubusercontent.com/u/43449361?v=4"}
-                alt={"avatar"}
-                width={32}
-                height={32}
-              />
-            </div>
-            {width! > 1024 && (
-              <>
-                <div className="flex flex-col items-start justify-center">
-                  <div className="text-white font-bold text-sm">
-                    Harun Doğdu <span>🇹🇷</span>
+          {data?.currenUser && (
+            <div className="flex gap-5 items-center justify-center rounded-full cursor-pointer hover:bg-neutral-800 hover:bg-opacity-70 mb-10 transition-colors p-2">
+              <div>
+                <Image
+                  src={data?.currentUser?.profileImage || "/default-avatar.jpg"}
+                  alt={"avatar"}
+                  width={32}
+                  height={32}
+                  className="rounded-full h-8 w-8 object-contain object-center bg-white"
+                />
+              </div>
+              {width! > 1024 && data?.currentUser?.email && (
+                <>
+                  <div className="flex flex-col items-start justify-center">
+                    <div className="text-white font-bold text-sm">
+                      {data?.currentUser?.name}
+                    </div>
+                    <div className="text-gray-500 text-sm">
+                      @{data?.currentUser?.username}
+                    </div>
                   </div>
-                  <div className="text-gray-500 text-sm">@harunndogdu</div>
-                </div>
-                <div>
-                  <RiMoreFill size={24} color="#fff" />
-                </div>
-              </>
-            )}
-          </div>
+                  <div>
+                    <RiMoreFill size={24} color="#fff" />
+                  </div>
+                </>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
