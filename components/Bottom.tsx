81ca071a36d31832797276f979e17eadb39470cd
Atahan <atahantutar69@gmail.com>
2023-04-07 03:53:54 +0300

    refactoring: edited in bottombar

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -24,52 +24,58 @@ const Bottom: FC<IModalProps> = ({ disabled = false }) => {
 
   const bodytittle = (
     <p className="text-white">
-      <span className="mr-2">Dont miss whats going on</span>
+      <span className="mr-2 font-bold">Don’t miss what’s happening</span>
     </p>
   );
 
   const bodycontent = (
     <p className="text-white">
-      <span className="mr-2">Twitter users will be the first to know..</span>
+      <span className="mr-2 font-bold">
+        People on Twitter are the first to know.
+      </span>
     </p>
   );
 
   return (
     <>
-      <div className="flex justify-between sticky overflow-x-hidden overflow-y-auto  inset-0 z-0 outline-none focus:outline-none  bottom-0">
-        <div className="flex justify-between  h-full w-full  lg:h-auto">
+      <div className="flex  sticky overflow-x-hidden overflow-y-auto  inset-0 z-0 outline-none focus:outline-none  bottom-0 shadow-[0_0px_9px_0px_rgba(255,255,255,0.6)]  ">
+        <div className="flex justify-center item-center  h-full w-full  lg:h-auto ">
           <div
-            className=" flex px-8 justify-between h-full w-full   grid-row-3"
+            className=" flex px-8 justify-evenly h-full w-full p-3 "
             style={{
               backgroundColor: ColorUtils.colors.purple,
             }}
           >
-            <div className=" flex items-center mx-8 p-2 pt-2 grid ">
+            <div className=" flex justify-between grid">
               <div className="text-2xl font-semibold text-white">
                 {bodytittle}
               </div>
               <div className="flex   ">{bodycontent}</div>
             </div>
 
-            <div className="mx-8 flex p-4">
-              <div>
-                <div className="flex items-center justify-end">
+            <div className="flex ">
+              <div className="flex items-center justify-end">
+                <div className="flex items-center grid-col-2">
                   {/*Button*/}
-                  <div className="flex flex-col gap-4 p-4  ">
+                  <div className="flex flex-col p-2 font-bold">
                     <Button
-                      label={"login"}
-                      disabled={disabled}
-                      secondary
-                      large
+                      label="Log in"
+                      size="lg"
+                      border="2"
+                      borderColor={ColorUtils.colors.white}
+                      bgColor={ColorUtils.colors.purple}
+                      color={ColorUtils.colors.white}
                       onClick={handleLoginClick}
                     />
                   </div>
-                  <div className="flex flex-col gap-4 p-4 ">
+                  <div className="flex flex-col p-2 font-bold">
                     <Button
-                      label={"register"}
-                      disabled={disabled}
-                      secondary
-                      large
+                      label="Sign up"
+                      border="border-2 "
+                      borderColor="border-rose-500"
+                      size="lg"
+                      bgColor={ColorUtils.colors.white}
+                      color={ColorUtils.colors.black}
                       onClick={handleRegisterClick}
                     />
                   </div>
