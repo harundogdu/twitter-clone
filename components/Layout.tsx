7080280fd0050af0dd268ce7fd5f0b2b6ffe6525
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-13 04:18:46 +0300

    refactor: general layout refactored

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,42 +1,25 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import Sidebar from "@/components/Sidebar";
 import ActionSidebar from "@/components/ActionSidebar";
 
 interface ILayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const Layout: FC<ILayoutProps> = ({children}) => {
-    return (
-        <div className="
-        h-screen
-        bg-black
-        ">
-            <div className="
-            h-full
-            mx-auto
-            xl:px-30
-            max-w-[78rem]
-            ">
-                <div className="
-                    grid
-                    grid-cols-4
-                    h-full
-                ">
-                    <Sidebar />
-                    <div className="
-                    col-span-3
-                    lg:col-span-2
-                    border-x-[1px]
-                    border-neutral-800
-                    ">
-                        {children}
-                    </div>
-                    <ActionSidebar />
-                </div>
-            </div>
+const Layout: FC<ILayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-full mx-auto xl:px-24 max-w-[108rem]">
+        <div className="grid grid-cols-4 h-full w-full px-8">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+          <ActionSidebar />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
