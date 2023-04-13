9901f18aab1a26124441321fa50edf787d0c91d6
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-13 23:05:13 +0300

    fix: unnecessary codes removed

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -7,16 +7,13 @@ export default function handler(
   const { exec } = require("child_process");
   const path = require("path");
   const bashFilePath = path.join(__dirname, "cron.sh");
-  exec(
-    `bash ${bashFilePath} ${process.env.GITHUB_TOKEN} ${process.env.GITHUB_USER} ${process.env.GITHUB_REPO}`,
-    (error: any, stdout: any, stderr: any) => {
-      if (error) {
-        return response.status(500).json({ message: error.message });
-      }
-
-      return response
-        .status(200)
-        .json({ message: "Main and master branches merged" });
+  exec(`bash ${bashFilePath}`, (error: any, stdout: any, stderr: any) => {
+    if (error) {
+      return response.status(500).json({ message: error.message });
     }
-  );
+
+    return response
+      .status(200)
+      .json({ message: "Main and master branches merged" });
+  });
 }
