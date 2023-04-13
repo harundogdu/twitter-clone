efa864336ba54c4c43026940f1f73152827af23e
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-13 20:51:52 +0300

    feat: error handling improved in fetcher

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const fetcher = async (url: string) => {
-  const response = await axios.get(url);
-  return response.data;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
 };
 
 export default fetcher;
