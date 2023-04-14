d7f624c4e8cb45a335b80a48816c1167a3c77c02
Atahan <atahantutar69@gmail.com>
2023-04-14 03:30:02 +0300

    feat: password icon visibility development.

--- components/shared/Input.tsx
+++ components/shared/Input.tsx
@@ -1,4 +1,6 @@
-import React, { FC, HTMLInputTypeAttribute } from "react";
+import React, { FC, HTMLInputTypeAttribute, useState } from "react";
+
+import { RiEyeLine, RiEyeOffLine } from "react-icons/ri";
 
 interface InputProps {
   type: HTMLInputTypeAttribute;
@@ -15,15 +17,34 @@ const Input: FC<InputProps> = ({
   value = "",
   onChange = () => {},
 }) => {
+  const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(false);
+
+  const onPasswordChangeVisibility = () => {
+    setIsPasswordHidden((current) => !current);
+  };
+
+  const renderType =
+    type === "password" ? (isPasswordHidden ? "text" : "password") : type;
+
   return (
-    <input
-      type={type}
-      className={`border border-gray-800 rounded-sm p-4 w-full focus:outline-none focus:ring-1 focus:ring-primary-main focus:border-transparent bg-transparent text-white`}
-      placeholder={placeholder}
-      disabled={disabled}
-      value={value}
-      onChange={onChange}
-    />
+    <div className="relative">
+      <input
+        type={renderType}
+        className={`border border-gray-800 rounded-sm p-4 w-full focus:outline-none focus:ring-1 focus:ring-primary-main focus:border-transparent bg-transparent text-white`}
+        placeholder={placeholder}
+        disabled={disabled}
+        value={value}
+        onChange={onChange}
+      />
+      {type === "password" ? (
+        <span
+          onClick={onPasswordChangeVisibility}
+          className="text-primary-main absolute top-1/2 right-4 -translate-y-1/2 select-none cursor-pointer "
+        >
+          {isPasswordHidden ? <RiEyeOffLine /> : <RiEyeLine />}
+        </span>
+      ) : null}
+    </div>
   );
 };
 
