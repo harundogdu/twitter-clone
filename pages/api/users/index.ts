c3095dcbb30db2fb5f746c898238b91a88f154ee
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-27 23:27:58 +0300

    feat: recommending user exclude yourself improved

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/libs/prismadb";
+import serverAuth from "@/libs/serverAuth";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,11 +12,18 @@ export default async function handler(
   }
 
   try {
+    const { currentUser } = await serverAuth(req, res);
+
     const users = await prisma.user.findMany({
       orderBy: {
         createdAt: "desc",
       },
       take: 3,
+      where: {
+        id: {
+          not: currentUser.id,
+        },
+      },
     });
 
     return res.status(200).json(users);
