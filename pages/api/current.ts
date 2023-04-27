8af0b2ac340c684b2e80ee03c7bbd18b999c5290
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-27 23:28:14 +0300

    fix: currentUser object fixed

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -11,10 +11,7 @@ export default async function handler(
 
   try {
     const { currentUser } = await serverAuth(req, res);
-    return res.status(200).json({
-      success: false,
-      currentUser,
-    });
+    return res.status(200).json(currentUser);
   } catch (error: any) {
     console.log(error);
     return res.status(401).end();
