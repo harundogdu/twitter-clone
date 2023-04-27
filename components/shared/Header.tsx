72dc9003db36ff42828c3cb8d97c91dba5803811
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-27 23:27:21 +0300

    feat: header component created

---
+++ components/shared/Header.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+import { FC, useCallback } from "react";
+
+import { RiArrowLeftLine } from "react-icons/ri";
+
+interface IHeaderProps {
+  showBackArrow?: boolean;
+  label: string;
+}
+
+const Header: FC<IHeaderProps> = ({ label, showBackArrow = false }) => {
+  const router = useRouter();
+
+  const handleBackClick = useCallback(() => {
+    if (Boolean(router.back())) {
+      return router.back();
+    }
+    router.push("/");
+  }, [router]);
+
+  return (
+    <div className="flex items-center border-b border-b-neutral-800 p-4 space-x-2">
+      {showBackArrow ? (
+        <RiArrowLeftLine
+          className="text-gray-300 hover:text-white cursor-pointer mx-1 hover:bg-neutral-800 hover:bg-opacity-70 rounded-full transition-colors"
+          size={24}
+          onClick={handleBackClick}
+        />
+      ) : null}
+      <h1 className="text-xl font-bold text-white">{label}</h1>
+    </div>
+  );
+};
+
+export default Header;
