5402369a4a66e4bffc4991859e695ef167f61479
Harun Doğdu <Harundogdu06@gmail.com>
2023-05-04 02:01:51 +0300

    feat: post feed component created

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,115 @@
+import React, { FC, useCallback, useMemo } from "react";
+import Avatar from "../Avatar";
+
+import { formatDistanceToNowStrict } from "date-fns";
+import useLoginModal from "@/hooks/useLoginModal";
+import { useRouter } from "next/router";
+
+import { RiChat3Line, RiHeart3Line } from "react-icons/ri";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+interface IPostFeedProps {
+  userId: string;
+  data: Record<string, any>;
+}
+
+const PostFeed: FC<IPostFeedProps> = ({ data, userId }) => {
+  const loginModal = useLoginModal();
+
+  const router = useRouter();
+  const { data: isLoggedIn } = useCurrentUser();
+
+  const goToUser = useCallback(
+    (event: React.MouseEvent<HTMLHeadingElement>) => {
+      event.stopPropagation();
+
+      router.push(`/users/${data?.user?.id}`);
+    },
+    [router, data?.user?.id]
+  );
+
+  const goToPost = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      if (isLoggedIn) {
+        router.push(`/posts/${data?.id}`);
+      }
+    },
+    [data?.id, router, isLoggedIn]
+  );
+
+  const onComment = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+      if (!isLoggedIn) {
+        return loginModal.onOpen();
+      }
+    },
+    [isLoggedIn, loginModal]
+  );
+
+  const onLike = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      event.stopPropagation();
+      if (!isLoggedIn) {
+        return loginModal.onOpen();
+      }
+    },
+    [isLoggedIn, loginModal]
+  );
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data?.createdAt]);
+
+  return (
+    <div
+      className="border-neutral-800 p-4 border-b transition hover:bg-neutral-900 cursor-pointer"
+      onClick={goToPost}
+    >
+      <div className="flex items-start gap-4">
+        <Avatar userId={userId} size="small" />
+        <div className="flex flex-col">
+          <div className="flex gap-2">
+            <h5
+              className="text-white font-semibold cursor-pointer hover:underline"
+              onClick={goToUser}
+            >
+              {data.user.name}
+            </h5>
+            <h6
+              className="text-neutral-500 cursor-pointer hover:underline"
+              onClick={goToUser}
+            >
+              @{data.user.username}
+            </h6>
+            <span className="text-neutral-500">·</span>
+            <span className="text-neutral-500">{createdAt}</span>
+          </div>
+          <p className="text-white">{data.body}</p>
+          <div className="flex gap-4">
+            <div
+              className="mt-2 flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-primary-main"
+              onClick={onComment}
+            >
+              <RiChat3Line size={18} />
+              <p>{data.Comment.length || 0}</p>
+            </div>
+            <div
+              className="mt-2 flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+              onClick={onLike}
+            >
+              <RiHeart3Line size={18} />
+              <p>{data.Comment.length || 0}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostFeed;
