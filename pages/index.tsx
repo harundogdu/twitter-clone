f81d6178902fc4abc529df5f4405838feeb7a014
Harun Doğdu <Harundogdu06@gmail.com>
2023-05-04 02:03:20 +0300

    feat: post form and feed used to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Header from "@/components/shared/Header";
 
+import PostForm from "@/components/PostForm";
+import PostFeeds from "@/components/posts/PostFeeds";
+
 export default function Home() {
   return (
     <main className="text-white">
       <Header label="Home" />
+      <PostForm placeholder="Whats happening?" />
+      <PostFeeds />
     </main>
   );
 }
