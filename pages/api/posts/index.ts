44da97eae6a7e0dd5904a405836968a15c62869e
Harun Doğdu <Harundogdu06@gmail.com>
2023-05-04 02:03:46 +0300

    feat: posts api created

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,65 @@
+import serverAuth from "@/libs/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@/libs/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET" && req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  try {
+    switch (req.method) {
+      case "POST":
+        const { currentUser } = await serverAuth(req, res);
+        const { body } = req.body;
+
+        const post = await prisma.post.create({
+          data: {
+            body,
+            userId: currentUser.id,
+          },
+        });
+
+        return res.status(201).json(post);
+      case "GET":
+      default:
+        const { userId } = req.query;
+
+        let posts = [];
+
+        if (userId && typeof userId === "string") {
+          posts = await prisma.post.findMany({
+            where: {
+              userId,
+            },
+            include: {
+              user: true,
+              Comment: true,
+            },
+            orderBy: {
+              createdAt: "desc",
+            },
+          });
+        } else {
+          posts = await prisma.post.findMany({
+            include: {
+              user: true,
+              Comment: true,
+            },
+            orderBy: {
+              createdAt: "desc",
+            },
+          });
+        }
+
+        return res.status(200).json(posts);
+    }
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).end();
+  }
+}
