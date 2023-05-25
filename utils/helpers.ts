c75747bada784e815a473bf2ee534b91f24bd36a
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-05-25 23:38:13 +0300

    feat: utils structure fixed

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,4 +1,25 @@
-export const validateEmail = (email: string): boolean => {
+const validateEmail = (email: string): boolean => {
   const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return emailRegex.test(email);
 };
+
+//utils.js
+/**
+ *@param {*} value
+ *@return {boolean}
+ */
+
+ const isNullOrUndefined = (value: any) => {
+  return value === null || value === undefined;
+};
+
+/**
+ * @param {*} value
+ * @return {boolean}
+ */
+
+const isNullOrEmpty = (value: any) => {
+  return value === null || value === "" || value === undefined;
+};
+
+export { isNullOrUndefined, isNullOrEmpty, validateEmail };
