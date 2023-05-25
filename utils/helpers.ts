1de97430c0ee5a0c97a4edacac0985579806d090
Atahan <atahantutar69@gmail.com>
2023-05-26 00:33:58 +0300

    feat: bio and post tag redirect

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,5 @@
+import router from "next/router";
+
 export const validateEmail = (email: string): boolean => {
   const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return emailRegex.test(email);
@@ -6,21 +8,36 @@ export const validateEmail = (email: string): boolean => {
 export const controlLink = (text: string): string => {
   const userRegex = /@(\w+)/g;
   const urlRegex = /(?<!href=["']|["']>)\b\S+\.com\/\S+\b(?![^<]*?<\/a>)/g;
-  var newText = text;
+  let newText = text;
 
   if (userRegex.test(text)) {
     newText = newText.replace(
       userRegex,
-      ' <a  href="$1"  style="color:#1D9BF0">@$1</a>'
+      `<div  style="color:#1D9BF0;display:inline-block; "  class="user-link hover:underline " data-username="$1">@$1</div>`
     );
   }
 
   if (urlRegex.test(text)) {
-    newText = newText.replace(
-      urlRegex,
-      '<a href="https://$&" style="color:#1D9BF0" >$&</a>'
-    );
+    newText = newText.replace(urlRegex, (match: string, domain: string) => {
+      const modifiedLink = match.replace(/https?:\/\/(?:www\.)?/i, "");
+      return `<a href="${match}" target="_blank" style="color:#1D9BF0" class="hover:underline">${modifiedLink}</a>`;
+    });
   }
+
+  document.addEventListener("click", (event) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("user-link")) {
+      const username = target.getAttribute("data-username");
+      if (username) {
+        onClick(username);
+      }
+    }
+  });
+
+  const onClick = (username: string) => {
+    const url = `/users/${username}`;
+    router.push(url);
+  };
 
   return newText;
 };
