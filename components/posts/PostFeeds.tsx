56d88e877abbc9abf813ab8584974cc6c8d8d5eb
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-05-25 23:13:48 +0300

    feat: loading structure on pages

--- components/posts/PostFeeds.tsx
+++ components/posts/PostFeeds.tsx
@@ -5,14 +5,17 @@ import usePosts from "@/hooks/usePosts";
 import PostFeed from "./PostFeed";
 import useUser from "@/hooks/useUser";
 
+import Loading from "@/components/shared/Loading";
+
 interface IPostFeedsProps {
   userId?: string;
   username?: string;
 }
 
 const PostFeeds: FC<IPostFeedsProps> = ({ userId, username }) => {
-  const { data: posts = [] } = usePosts(userId as string);
+  const { data: posts = [], isLoading } = usePosts(userId as string);
 
+  if (isLoading) return <Loading />;
   return (
     <>
       {posts.map((post: Record<string, any>) => (
