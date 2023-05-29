aebf1e49e1549c35b4dc89224265e590f26a0c39
ahmetenesbahar <enesbahar2303@gmail.com>
2023-05-30 02:33:16 +0300

    feat: 404 pages styling

---
+++ pages/404.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import ColorUtils from "@/base/colors";
+import SpaceUtils from "@/base/spaces";
+
+import Button from "@/components/shared/Button";
+
+const Custom404 = () => {
+  return (
+    <div className="flex justify-center items-center flex-col mt-24">
+      <p style={{ color: "#71767B" }}>
+        Hmm...this page doesn't exist. Try searching for something else.
+      </p>
+      <button
+        style={{
+          background: "#1f9bf0",
+          color: "#fff",
+          padding: "0.3rem 1rem",
+          marginTop: "1.5rem",
+          fontWeight: 700,
+          borderRadius: "16px",
+        }}
+      >
+        <Link href="/">Search</Link>
+      </button>
+    </div>
+  );
+};
+export default Custom404;
