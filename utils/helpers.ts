f2379e06502ea8dded66aefc4458ca40294dd9e5
Harun Doğdu <Harundogdu06@gmail.com>
2023-05-30 01:03:44 +0300

    refactor: url viewer and definition changed

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,28 +1,90 @@
 import router from "next/router";
 
+import { REGEX } from "@/utils/constants";
+
 const validateEmail = (email: string): boolean => {
   const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return emailRegex.test(email);
 };
 
+const clearProtocol = (url: string): string => {
+  return url.replace(/(^\w+:|^)\/\//, "").replace("www.", "");
+};
+
+const getURLText = (url: string) => {
+  if (url.includes("youtu.be")) {
+    url = url.replace("youtu.be/", "https://youtube.com/watch?v=");
+  }
+
+  try {
+    const match = url.match(REGEX.URL_REGEX);
+    if (!match) return false;
+    const website = match[0];
+    if (!website) return false;
+    return website;
+  } catch {
+    return false;
+  }
+};
+
+const handleURL = (url: string) => {
+  if (!url) return;
+  if (
+    url.includes("https") ||
+    url.includes("http") ||
+    url.includes("www") ||
+    REGEX.URL_REGEX.test(url) ||
+    REGEX.YOUTUBE_URL_REGEX.test(url)
+  ) {
+    if (!getURLText(url)) return;
+    if (url.includes("youtu.be")) {
+      return url.replace("youtu.be/", "youtube.com/watch?v=");
+    }
+    const newURL = getURLText(url);
+    if (!newURL) return;
+    return newURL;
+  }
+  return `https://${url}`;
+};
+
 const controlLink = (text: string): string => {
-  const userRegex = /@(\w+)/g;
-  const urlRegex = /(?<!href=["']|["']>)\b\S+\.com\/\S+\b(?![^<]*?<\/a>)/g;
+  let formattedText = text;
 
-  let newText = text;
-
-  if (userRegex.test(text)) {
-    newText = newText.replace(
-      userRegex,
-      `<div style="color:#1D9BF0" class="user-link" data-username="$1">@$1</div>`
+  if (REGEX.USER_REGEX.test(text)) {
+    formattedText = formattedText.replace(
+      REGEX.USER_REGEX,
+      `<span class="text-primary-main user-link cursor-pointer" data-username="$1">@$1</span>`
     );
   }
 
-  if (urlRegex.test(text)) {
-    newText = newText.replace(urlRegex, (match: string, domain: string) => {
-      const modifiedLink = match.replace(/https?:\/\/(?:www\.)?/i, "");
-      return `<a href="${match}" target="_blank" style="color:#1D9BF0">${modifiedLink}</a>`;
+  if (handleURL(text)) {
+    const match = text.match(REGEX.URL_REGEX);
+    const allMatches = match?.[0].split(" ");
+
+    let urlText = "";
+    allMatches?.forEach((match) => {
+      let innerFinalURL = match;
+      if (!match.includes("http") && !match.includes("https")) {
+        innerFinalURL = `https://${match}`;
+      }
+
+      urlText += innerFinalURL + " ";
     });
+
+    formattedText = formattedText.replace(
+      REGEX.URL_REGEX,
+      urlText
+        .split(" ")
+        .map((text) => {
+          return `<a id="external-url" href="${text}" target="_blank" class="text-primary-main cursor-pointer">${clearProtocol(
+            text
+          )}</a>`;
+        })
+        .filter((text, i, arr) => arr.indexOf(text) === i)
+        .join(" ")
+    );
+
+    return formattedText;
   }
 
   document.addEventListener("click", (event) => {
@@ -40,7 +102,7 @@ const controlLink = (text: string): string => {
     router.push(url);
   };
 
-  return newText;
+  return formattedText;
 };
 
 const isNullOrUndefined = (value: any) => {
