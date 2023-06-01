7ec138c64c4413f1cd17b610afcf00b43b57405f
ahmetenesbahar <enesbahar2303@gmail.com>
2023-06-01 19:35:31 +0300

    feat: input names goes up when focus

--- components/shared/Input.tsx
+++ components/shared/Input.tsx
@@ -35,7 +35,7 @@ const Input: FC<InputProps> = ({
   const placeholderText = "Email or Username";
 
   const inputControl = (type: string, value: string): string => {
-    let borderColor: string = "focus:ring-primary-main ";
+    let borderColor: string = "focus:ring-transparent "; //!
 
     if (isNullOrEmpty(value) && isNullOrUndefined(value)) {
       borderColor = "focus:ring-red-600 border-gray-800";
@@ -68,18 +68,40 @@ const Input: FC<InputProps> = ({
     type === "password" ? (isPasswordHidden ? "text" : "password") : type;
 
   return (
-    <div className="relative">
+    <div className="relative group">
       <input
         type={renderType}
-        placeholder={placeholder}
+        placeholder=" "
         disabled={disabled}
         value={value}
         onChange={onChange}
         className={`border border-gray-800 rounded-sm p-4 w-full focus:outline-none focus:ring-1 ${inputControl(
           type,
           value
-        )} focus:border-transparent bg-transparent text-white`}
+        )} focus:border-transparent bg-transparent text-white peer`}
       />
+
+      <label
+        className="absolute  left-[9px] top-px text-gray-500 transition-all duration-300 px-1 transform -translate-y-1/2 pointer-events-none 
+      peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base group-focus-within:!top-px group-focus-within:!text-sm focus:text-blue-500"
+      >
+        {placeholder}
+      </label>
+
+      <fieldset
+        className="inset-0 absolute border border-gray-800  rounded pointer-events-none mt-[-9px] invisible peer-placeholder-shown:visible 
+group-focus-within:!border-blue-500 group-focus-within:border-2"
+      >
+        <legend className="ml-2 px-0 text-sm transition-all duration-300 invisible max-w-[0.01px] group-focus-within:max-w-full group-focus-within:px-1 whitespace-nowrap">
+          {placeholder}
+        </legend>
+      </fieldset>
+
+      <fieldset className="inset-0 absolute border border-gray-800 rounded pointer-events-none mt-[-9px] visible peer-placeholder-shown:invisible  group-focus-within:border-2 group-focus-within:!border-blue-500 group-hover:border-gray-700">
+        <legend className="ml-2 text-sm invisible px-1 max-w-full whitespace-nowrap">
+          {placeholder}
+        </legend>
+      </fieldset>
       {value &&
       !validateEmail(value) &&
       type === "text" &&
