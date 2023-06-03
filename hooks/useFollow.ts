229fb1e5e2df1b649a6f16666421c32bdbd80670
Harun Doğdu <Harundogdu06@gmail.com>
2023-06-03 18:16:11 +0300

    feat: useFollow hooks created

---
+++ hooks/useFollow.ts
@@ -0,0 +1,67 @@
+import { useCallback, useMemo } from "react";
+
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+import useCurrentUser from "./useCurrentUser";
+import useLoginModal from "./useLoginModal";
+import useUser from "./useUser";
+
+const useFollow = (userName: string) => {
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { data: fetchedUser, mutate: mutateFetchedUser } = useUser(userName);
+
+  const loginModal = useLoginModal();
+
+  const isFollowing = useMemo(() => {
+    const list = currentUser?.followingIds || [];
+    return list.includes(fetchedUser?.id || "");
+  }, [currentUser, fetchedUser]);
+
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      loginModal.onOpen();
+      return;
+    }
+
+    try {
+      let request;
+
+      if (isFollowing) {
+        request = () =>
+          axios.delete(`/api/follow`, {
+            data: {
+              username: fetchedUser?.username,
+            },
+          });
+      } else {
+        request = () =>
+          axios.post(`/api/follow`, {
+            username: fetchedUser?.username,
+          });
+      }
+
+      await request();
+
+      mutateCurrentUser();
+      mutateFetchedUser();
+    } catch (error: any) {
+      console.log(error);
+      toast.error("Failed to follow user");
+    }
+  }, [
+    currentUser,
+    fetchedUser?.username,
+    isFollowing,
+    loginModal,
+    mutateCurrentUser,
+    mutateFetchedUser,
+  ]);
+
+  return {
+    isFollowing,
+    toggleFollow,
+  };
+};
+
+export default useFollow;
