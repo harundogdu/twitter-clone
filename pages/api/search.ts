36d1789e8393794f0ef7e16d1c95052a170bd0f4
Harun Doğdu <Harundogdu06@gmail.com>
2023-06-27 19:58:41 +0300

    feat: search api developed by username and by name

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -16,19 +16,39 @@ export default async function handler(
       throw new Error("Invalid username or name");
     }
 
-    const searchedUsers = await prisma.user.findMany({
+    const searchedUsersByUserName = await prisma.user.findMany({
       where: {
         username: {
           contains: username,
+          mode: "insensitive",
         },
       },
     });
 
-    if (!searchedUsers) {
-      throw new Error("User not found");
+    const searchedUsersByName = await prisma.user.findMany({
+      where: {
+        name: {
+          contains: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    const searchedUserList = searchedUsersByName.concat(
+      searchedUsersByUserName
+    );
+
+    const searchedUserListUnique = searchedUserList.filter(
+      (user, index, self) =>
+        index ===
+        self.findIndex((t) => t.username === user.username && t.id === user.id)
+    );
+
+    if (!searchedUserListUnique) {
+      throw new Error("No users found");
     }
 
-    return res.status(200).json({ users: searchedUsers });
+    return res.status(200).json({ users: searchedUserListUnique });
   } catch (error: any) {
     return res.status(401).end();
   }
