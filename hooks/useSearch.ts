59cdad70ac93c217e6d79ef016b2904ab0603f68
ahmetenesbahar <enesbahar2303@gmail.com>
2023-06-27 04:28:29 +0300

    feat: Add useSearch hook for user search functionality

---
+++ hooks/useSearch.ts
@@ -0,0 +1,28 @@
+import { useCallback } from "react";
+import axios from "axios";
+
+const useSearch = () => {
+  const searchUsers = useCallback(async (username: string) => {
+    if (username === "" || username === undefined || username === null) {
+      return [];
+    }
+    try {
+      const { data } = await axios.get(`/api/search`, {
+        params: {
+          username: username.toLowerCase(),
+          name: username.toLowerCase(),
+        },
+      });
+
+      return data.users;
+    } catch (error: any) {
+      console.log(error);
+      return [];
+    }
+  }, []);
+
+  return {
+    searchUsers,
+  };
+};
+export default useSearch;
