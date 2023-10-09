4f68f740f1468f283c57fce9e2d162868ab8a399
Harun Doğdu <Harundogdu06@gmail.com>
2023-10-09 14:36:49 +0300

    feat: notification method defined for the like any post

--- pages/api/likes.ts
+++ pages/api/likes.ts
@@ -34,6 +34,34 @@ export default async function handler(
       updatedLikes = updatedLikes.filter((id) => id !== currentUser.id);
     } else {
       updatedLikes.push(currentUser.id);
+
+      try {
+        const post = await prisma.post.findUnique({
+          where: {
+            id: postId,
+          },
+        });
+
+        if (post?.userId) {
+          await prisma.notification.create({
+            data: {
+              body: "Someone liked your post",
+              userId: post.userId,
+            },
+          });
+
+          await prisma.user.update({
+            where: {
+              id: post.userId,
+            },
+            data: {
+              hasNotification: true,
+            },
+          });
+        }
+      } catch (error: any) {
+        console.log(error);
+      }
     }
 
     const updatedPost = await prisma.post.update({
